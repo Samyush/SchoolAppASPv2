2988e06de338a2ce3759bc348c804f2f4f4a3621
Samyush <samyushmaharjan@gmail.com>
2021-11-13 21:43:53 +0545

    transfet to dotnet 6.0

--- SchoolAppASPv2.Identity/Data/ApplicationDbContext.cs
+++ SchoolAppASPv2.Identity/Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace SchoolAppASPv2.Identity.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
--- SchoolAppASPv2.Identity/Startup.cs
+++ SchoolAppASPv2.Identity/Startup.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using SchoolAppASPv2.Identity.Data;
+using SchoolAppASPv2.Identity.Models;
 //using 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace SchoolAppASPv2.Identity
 {
@@ -39,6 +42,67 @@ namespace SchoolAppASPv2.Identity
                 options.UseOpenIddict();
 
             });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = Claims.Role;
+                options.SignIn.RequireConfirmedAccount = false;
+            }
+                );
+
+            services.AddOpenIddict()
+                .AddCore(options =>
+                {
+                    options.UseEntityFrameworkCore()
+                    .UseDbContext<ApplicationDbContext>();
+                }).AddServer(options =>
+                {
+                    options.SetAuthorizationEndpointUris("/connect/authorize")
+                    .SetLogoutEndpointUris("/connect/logout")
+                    .SetTokenEndpointUris("connect/token")
+                    .SetIntrospectionEndpointUris("/connect/introspect")
+                    .SetUserinfoEndpointUris("/connect/userinfo");
+
+                    options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+                    options.AllowImplicitFlow()
+                                              .AllowAuthorizationCodeFlow()
+                                              .AllowClientCredentialsFlow();
+
+                    // Register the signing and encryption credentials.
+                    //options.AddDevelopmentEncryptionCertificate()
+                    //       .AddDevelopmentSigningCertificate();
+
+                    options.AddEphemeralSigningKey()
+                         .AddEphemeralEncryptionKey();
+
+
+                    // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                //    options.UseAspNetCore()
+                //    .DisableTransportSecurityRequirement()
+                //            .EnableTokenEndpointPassthrough()
+                //            .EnableAuthorizationEndpointPassthrough()
+                //           .EnableLogoutEndpointPassthrough()
+                //           .EnableUserinfoEndpointPassthrough()
+                //           .EnableStatusCodePagesIntegration();
+                //}).AddValidation(options =>
+                //{
+                //    // Import the configuration from the local OpenIddict server instance.
+                //    options.UseLocalServer();
+
+                //    // Register the ASP.NET Core host.
+                //    options.UseAspNetCore();
+
+
+                }
+                );
 
             services.AddRazorPages();
 
