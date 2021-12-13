8d02cfd1eae0e9f7833e57da180a75731680e1b5
Samyush <samyushmaharjan@gmail.com>
2021-12-13 21:44:40 +0545

    work over src

---
+++ SchoolAppASPv2.Core/Entities/UserPass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAppASPv2.Core.Entities
+{
+    [Table("userpass", Schema = "dbo")]
+    public class UserPass
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
